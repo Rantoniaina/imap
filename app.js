b963240ab779b3604472e8983b03f8d2e4606f5a
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2021-01-07 16:16:23 +0300

    Certification attempts

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
+// var https = require("https");
+// var fs = require("fs");
 
 var app = express();
 
@@ -13,6 +15,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", require("./routes/web"));
 app.use("/api", require("./routes/api"));
 
+// const httpsOptions = {
+//   key: fs.readFileSync("./security/key.pem"),
+//   cert: fs.readFileSync("./security/cert.pem"),
+// };
+
 app.listen(app.get("port"), function () {
   console.log("Server started on port " + app.get("port"));
 });
+
+// https.createServer(httpsOptions, app).listen(app.get("port"), () => {
+//   console.log("Server started on port " + app.get("port"));
+// });
